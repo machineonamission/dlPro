f7f9d7651df42182fb4f869437aae851d4108704
Melody <machineonamission@gmail.com>
2025-07-14 10:23:19 -0500

    rename

--- classic_worker_patch.js
+++ classic_worker_patch.js
@@ -8,14 +8,6 @@ const NativeWorker = self.Worker;
 function PatchedWorker(scriptURL, options = {}) {
     // Always force classic mode
     const opts = Object.assign({}, options, {type: 'classic'});
-    // worker urls need to be trusted i guess
-    // if (trustedTypes && trustedTypes.createPolicy) {
-    //     const policy = trustedTypes.defaultPolicy || trustedTypes.createPolicy('ytdlpxtn', {
-    //         // Here we simply pass through—the blob URL is already trusted by you.
-    //         createScriptURL: url => url,
-    //     });
-    //     scriptURL = policy.createScriptURL(scriptURL);
-    // }
     return new NativeWorker(scriptURL, opts);
 }
 
