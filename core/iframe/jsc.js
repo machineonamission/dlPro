064cda4621e215cc52fa943cb68650da83b50203
Melody <machineonamission@gmail.com>
2025-10-28 16:02:58 -0500

    FUCK YOU FIREFOX

--- core/iframe/jsc.js
+++ core/iframe/jsc.js
@@ -3,7 +3,9 @@ function sandbox_run_js(code) {
         code = code.replaceAll("await import('npm:", "await import('https://esm.sh/")
         console.debug("requesting sandboxed js execution", code);
         const iframe = document.createElement("iframe");
-        iframe.src = chrome.runtime.getURL("/core/iframe/sandbox.html");
+        iframe.src = chrome.runtime.getURL("/core/sandbox/sandbox.html");
+        // this ONE fucking line caused me an hour of debugging cause firefox doesnt support sandbox in the manifest FUCKING HELL
+        iframe.sandbox = "allow-scripts";
         iframe.style.display = "none";
         document.body.appendChild(iframe);
 
